af5e4617094d1ea351d1d866bc2333520ec0f2f4
Rodrigo Valladares Sánchez <rvalladares@inacap.cl>
2020-01-17 15:34:48 -0300

    Mod: cambio diseño Cards

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,8 +2,8 @@
 <div class="card-deck text-center">
     <div class="card" style="width: 18rem;">
         <div class="card-body">
-            <h5 class="card-title display-1">~{{$promedioDia}}</h5>
-            <p class="card-text text-muted">Promedio diario</p>
+            <h5 class="card-title display-4">Promedio diario</h5>
+            <p class="card-text display-1 text-muted">~{{$promedioDia}}</p>
         </div>
         <div class="card-footer">
             <a href="/registros" class="btn btn-outline-primary btn-block btn-lg">Ir a Registros</a>
@@ -11,8 +11,8 @@
     </div>
     <div class="card" style="width: 18rem;">
         <div class="card-body">
-            <h5 class="card-title display-1">{{$totalRegistros}}</h5>
-            <p class="card-text text-muted">Total registros</p>
+            <h5 class="card-title display-4">Total registros</h5>
+            <p class="card-text text-muted display-1">{{$totalRegistros}}</p>
             <div class="container">
                 <div class="row">
                     <div class="col-sm">
@@ -42,11 +42,11 @@
         <div class="card-body">
             <h5 class="card-title display-4">Ranking</h5>
             <ul class="list-unstyled text-left">
-                <li>#1 {{$ranking[0]->usuario}} ({{$ranking[0]->occurrences}} registros)</li>
-                <li>#2 {{$ranking[1]->usuario}} ({{$ranking[1]->occurrences}} registros)</li>
-                <li>#3 {{$ranking[2]->usuario}} ({{$ranking[2]->occurrences}} registros)</li>
-                <li>#4 {{$ranking[3]->usuario}} ({{$ranking[3]->occurrences}} registros)</li>
-                <li>#5 {{$ranking[4]->usuario}} ({{$ranking[4]->occurrences}} registros)</li>
+                <li>#1 {{$ranking[0]->usuario}} ({{$ranking[0]->total}} registros)</li>
+                <li>#2 {{$ranking[1]->usuario}} ({{$ranking[1]->total}} registros)</li>
+                <li>#3 {{$ranking[2]->usuario}} ({{$ranking[2]->total}} registros)</li>
+                <li>#4 {{$ranking[3]->usuario}} ({{$ranking[3]->total}} registros)</li>
+                <li>#5 {{$ranking[4]->usuario}} ({{$ranking[4]->total}} registros)</li>
             </ul>
         </div>
         <div class="card-footer">
