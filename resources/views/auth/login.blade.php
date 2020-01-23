f77b79b085befd3baf8bdaa6df892a74120f214c
Rodrigo Valladares Sánchez <rvalladares@inacap.cl>
2020-01-23 17:00:43 -0300

    Mod: labels 'Login' y 'Password' cambiado por 'Correo' y 'Clave'

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -114,14 +114,14 @@
             <input type="email" id="email" name="email"
                 class="form-control form-control-lg @error('email') is-invalid @enderror" placeholder="Login" required
                 autofocus />
-            <label for="email">Login</label>
+            <label for="email">Correo electronico</label>
             @error('email')<strong>{{ $message }}</strong>@enderror
         </div>
         <div class="form-label-group">
             <input type="password" id="password" name="password"
                 class="form-control form-control-lg @error('email') is-invalid @enderror" placeholder="Password"
                 required />
-            <label for="password">Password</label>
+            <label for="password">Clave</label>
         </div>
         <button class="btn btn-lg btn-primary btn-block" type="submit">Iniciar sesion</button>
         <p class="mt-5 mb-3 text-muted text-center">&copy; 2019-2020</p>
